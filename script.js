dc1e9b88ea9691bff96d6ac56d8f71149f834bda
kevinressler <kevin.ressler24@gmail.com>
2025-03-20 00:52:42 -0400

    stuff

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleButton = document.createElement("button");
+    toggleButton.innerText = "Toggle Dark Mode";
+    document.body.appendChild(toggleButton);
+
+    toggleButton.addEventListener("click", function () {
+        document.body.classList.toggle("dark-mode");
+    });
+
+});
 
